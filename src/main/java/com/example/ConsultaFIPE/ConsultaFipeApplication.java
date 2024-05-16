d39f5e99af646f86a85568272116a6d7bf2432e5
deivid94 <md031194@gmail.com>
2024-05-16 14:39:41 -0300

    Primeiro commit, criando estrutura do projeto

---
+++ src/main/java/com/example/ConsultaFIPE/ConsultaFipeApplication.java
@@ -0,0 +1,18 @@
+package com.example.ConsultaFIPE;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaFipeApplication  implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaFipeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
