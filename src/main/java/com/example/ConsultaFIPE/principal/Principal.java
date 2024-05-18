7b1eaf18f4fa089899abfc8978eb0ead9343e1c5
deivid94 <md031194@gmail.com>
2024-05-18 14:09:22 -0300

    Refactory: refatorando estrutura de classes do projeto

--- src/main/java/com/example/ConsultaFIPE/principal/Principal.java
+++ src/main/java/com/example/ConsultaFIPE/principal/Principal.java
@@ -8,50 +8,93 @@ import com.example.ConsultaFIPE.service.ConverteDados;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
     private final String  ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
-    private final String MARCAS = "/marcas";
+
     private  Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumindoApi = new ConsumoApi();
     private ConverteDados converteDadosParaJson = new ConverteDados();
 
 
     public void exibeMenu(){
+        int contador = 0;
+        while (contador != 4) {
+            var menu = """
+                       ***OPCOES DE VEICULOS ***
+                       -------------
+                       1-CARROS
+                       2-MOTOS
+                       3-CAMINHOES
+                       4- SAIR
+                       --------------
+                       QUAL TIPO DE VEICULO VOCE DESEJA CONSULTAR ?
+                       \n
+                    """;
+            System.out.println(menu);
 
-        var menu  = """
-                   **OPCOES**
-                   -------------
-                   carros
-                   motos
-                   caminhoes
-                   --------------
-                   DIGITE UMA DAS OPCOES PARA CONSULTAR
-                   \n
-                """;
-        System.out.println(menu);
-        var opcaoDigitadaPeloUsuario = leitura.nextLine().toLowerCase();
+            var opcaoDeVeiculo = leitura.nextLine();
+
+            switch (opcaoDeVeiculo) {
+                case "1":
+                    opcaoDeVeiculo = "carros/marcas";
+                    break;
+                case "2":
+                    opcaoDeVeiculo = "motos/marcas";
+                    break;
+
+                case "3":
+                    opcaoDeVeiculo = "caminhoes/marcas";
+                    break;
+                case "4":
+                    System.out.println("CONSULTAR ENCERRADA PELO USUARIO");
+                default:
+                    System.out.println("OPCAO INVALIDA. ESOCLHA AS OPCOES 1 A 4");
+                    break;
+            };
+
+                var json = consumindoApi.obterDados(ENDERECO + opcaoDeVeiculo);
+                var marcasVeiculos = converteDadosParaJson.obterlista(json, DadosMarcas.class);
+
+                marcasVeiculos.stream()
+                        .sorted(Comparator.comparing(DadosMarcas::codigo))
+                        .forEach(System.out::println);
 
 
-        var json = consumindoApi.obterDados(ENDERECO+opcaoDigitadaPeloUsuario+MARCAS);
-        var marcasVeiculos  = converteDadosParaJson.obterlista(json, DadosMarcas.class);
-
-        marcasVeiculos.stream()
-                .sorted (Comparator.comparing(DadosMarcas::codigo))
-                .forEach(System.out::println);
 
 
-        System.out.println("\n Escolha um codigo da marca que deseja visualizar alguem modelo: ");
-        var usuarioEscolheModelo = leitura.nextLine().toLowerCase();
-        System.out.println(usuarioEscolheModelo);
 
-        var consultaModeloNaApi = consumindoApi.obterDados(ENDERECO+opcaoDigitadaPeloUsuario+MARCAS+"/"+usuarioEscolheModelo+"/modelos");
-        //https://parallelum.com.br/fipe/api/v1/carros/marcas/7/modelos
-        System.out.println(consultaModeloNaApi);
+            System.out.println("\n Escolha um codigo da marca que deseja visualizar alguem modelo: ");
+            var usuarioEscolheModelo = leitura.nextLine().toLowerCase();
 
-        var modeloLista = converteDadosParaJson.obterDados(consultaModeloNaApi, DadosModelo.class);
-        System.out.println(modeloLista);
+            System.out.println("\n MODELOS DA MARCA ESCOLHIDA: ");
+            var consultaModeloNaApi = consumindoApi.obterDados(ENDERECO + opcaoDeVeiculo + "/" + usuarioEscolheModelo + "/modelos");
+            //https://parallelum.com.br/fipe/api/v1/carros/marcas/7/modelos
+            System.out.println(consultaModeloNaApi);
+
+            var modeloLista = converteDadosParaJson.obterDados(consultaModeloNaApi, DadosModelo.class);
+            modeloLista.modelos().stream()
+                    .sorted(Comparator.comparing(DadosMarcas::nome))
+                    .forEach(System.out::println);
+
+            System.out.println("\n INFORME  NOME DO VEICULO");
+            var nomeDoVeiculo = leitura.nextLine();
+            List<DadosMarcas> listaComTodosModelos = modeloLista.modelos().stream()
+                    .filter(m -> m.nome().toLowerCase().contains(nomeDoVeiculo.toLowerCase()))
+                    .collect(Collectors.toList());
+
+
+            System.out.println("\nMODELOS FILTRADOS");
+            listaComTodosModelos.forEach(System.out::println);
+            //https://parallelum.com.br/fipe/api/v1/carros/marcas/7/modelos/8314/anos
+
+        }
+
+
+
+
 
     }
 
